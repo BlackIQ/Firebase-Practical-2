8917f6ed613377a74a0af00a5e7f3ec7fd0ca226
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-10 22:22:12 +0330

    Fix Provider

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -4,21 +4,14 @@ import 'package:firebaseauth/app/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class Landing extends StatefulWidget {
-  @override
-  _LandingState createState() => _LandingState();
-}
-
-class _LandingState extends State<Landing> {
+class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    dynamic user = 'null';
-    // final user = Provider.of<User>(context);
+    final user = Provider.of<User>(context);
 
     if (user == null) {
       return Authenticate();
-    }
-    else {
+    } else {
       return Home();
     }
   }
