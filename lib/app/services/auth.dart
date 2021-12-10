6e02f927fbd3b10bf56fc67d06336e1bbc71dd6a
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-10 22:21:28 +0330

    Create anon login and logout

---
+++ lib/app/services/auth.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebaseauth/app/models/user.dart';
+
+class AuthenticationService {
+  // Instance Firebase
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Create user object based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    if (user != null) {
+      return User(uid: user.uid);
+    } else {
+      return null;
+    }
+    // return user != null ? User(uid: user.uid) : null;
+  }
+
+  // User
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
+
+  // SignIn Anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      return [
+        null,
+        e.toString(),
+      ];
+    }
+  }
+
+  // Register with Email
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      return [
+        null,
+        e.toString(),
+      ];
+    }
+  }
+}
