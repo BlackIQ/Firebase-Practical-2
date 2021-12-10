51de3071b39d3c7b484227201f061349db16fe1b
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-10 22:22:01 +0330

    Add Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:firebaseauth/app/models/user.dart';
 import 'package:firebaseauth/app/screens/landing.dart';
+import 'package:firebaseauth/app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Auth',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    return StreamProvider<User>.value(
+      value: AuthenticationService().user,
+      child: MaterialApp(
+        title: 'Firebase Auth',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: Landing(),
       ),
-      home: Landing(),
     );
   }
 }
