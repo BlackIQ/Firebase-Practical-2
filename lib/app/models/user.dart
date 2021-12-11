e0f634b7489524ddae6e34ad988ca1d010f2dfb5
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-11 16:44:47 +0330

    Add photo url and email

--- lib/app/models/user.dart
+++ lib/app/models/user.dart
@@ -3,9 +3,13 @@ class User {
     this.uid,
     this.profile,
     this.name,
+    this.email,
+    this.phone,
   });
 
   final uid;
   final profile;
   final name;
+  final email;
+  final phone;
 }
